6b96be9aeb00f96ca061b6b3822a4c42b5c4874f
ShiYang1101 <ShiYang1101@users.noreply.github.com>
2022-07-02 10:51:23 +0000

    deploy: fa62c3c84644262e9b222cc9735091c1ddb06502

--- notebooks/spectogram.py
+++ notebooks/spectogram.py
@@ -1,11 +1,21 @@
 import librosa
 import glob
 import os 
+import math
+import numpy as np
 
 def generate_spec(path):
     file, sr = librosa.load(path)
     y = librosa.stft(file)
     return y
+
+def truncate_spec(sig, max_len):
+    if sig.shape[0] < max_len:
+        to_pad = max_len - sig.shape[0]
+        left_pad = math.floor(to_pad/2)
+        right_pad = math.ceil(to_pad/2)
+        return np.pad(sig, ((0, 0), (left_pad, right_pad)))
+
 
 def get_spec(df):
     path = glob.glob('**/_OrchideaSOL2020_release', recursive = True)
