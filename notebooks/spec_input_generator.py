6e0d56f337f23eb5743ede85911dbb05587e89f8
ShiYang1101 <shiyanglee1998@gmail.com>
2022-07-20 15:11:16 +0100

    update on 2conv and generator.py

--- notebooks/spec_input_generator.py
+++ notebooks/spec_input_generator.py
@@ -11,9 +11,9 @@ _path_to_npy = glob.glob('../data/**[!MACOSC]/*OrchideaSOL2020/', recursive=True
 def gen(train_df, noise_prob = 0.3, mask_prob = 0.3, preprocess = False, 
                 return_class = False):
     df = train_df.copy()
-    
+    freq = 1/df.groupby('_ins')['_ins'].transform('count')
     while True: 
-        tmp_df = df.sample(1)
+        tmp_df = df.sample(1, replace = True, weights = freq)
         path = os.path.join(dir_path, _path_to_npy, tmp_df['Path'].values[0])
         try:
             spec = spectrogram(np.load(os.path.splitext(path)[0] + '.npy', allow_pickle=True))
@@ -23,7 +23,7 @@ def gen(train_df, noise_prob = 0.3, mask_prob = 0.3, preprocess = False,
         #     spec.
         spec.spec = np.expand_dims(spec.spec, -1)
         yield (spec.spec if return_class == False else spec, 
-        np.reshape(tmp_df[list(set(df.columns) - set(['Path']))].values, (16,)))
+        np.reshape(tmp_df[list(sorted(set(df.columns) - set(['Path', '_ins'])))].values, (16,)))
     
 def gen_eval(test_df, noise_prob = 0.3, mask_prob = 0.3):
     df = test_df.copy()
@@ -35,4 +35,4 @@ def gen_eval(test_df, noise_prob = 0.3, mask_prob = 0.3):
         except:
             spec = spectrogram(path)
         spec.spec = np.expand_dims(spec.spec, -1)
-        yield (spec.spec, np.reshape(tmp_df[list(set(df.columns) - set(['Path']))].values, (16,)))
+        yield (spec.spec, np.reshape(tmp_df[list(sorted(set(df.columns) - set(['Path', '_ins'])))].values, (16,)))
